850932b88fa0eb5665105b2b8eeaf9293ee66993
Fernando Portella <59586318+fjportella@users.noreply.github.com>
2024-01-18 00:00:04 +0000

    Primeiros códigos python - variaveis e operadores

---
+++ 2.3 - Python/teste.py
@@ -0,0 +1,33 @@
+# Não há de fato Variáveis Constantes no Python. Mas podemos declarar em MAIÚSCULO para simbolzar constantes
+
+# Entrada de Dados
+# nome        = input("Digite o seu nome: ")
+# nascimento  = input("Qual seu nome de nascimento? ")
+# email       = input("Digite seu e-mail: ")
+# print("Meu nome é : ", nome, " meu ano de nascimento é: ", nascimento, " e meu e-mail é: ", email)
+
+#Tratamento de números no input
+from math import sqrt
+
+
+a = int(input("Digite um número: "))
+print(a + 5)
+
+#1 Operadores Aritiméticos
+## +   -   *   /
+
+#2 Operadores Relacionais
+## <   >   >=   <=   ==   !=   ===
+
+#2 Operadores Lógicos
+## &&   ||    !
+
+#4 Outros Operadores
+## potência **
+## raiz sqtr()
+## modulo (resto) %
+
+print(18%4)
+
+print(sqrt(4))
+
